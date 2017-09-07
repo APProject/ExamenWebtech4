20148cce4baf04fc72d71cbbb9b940c6ba100d4f
Achrafbt <achraf.bentaieb@student.ap.be>
2017-09-07 17:04:02 +0200

    laatste commit
    kleine aanpassingen, mongo client in views py en url.py aangepast

--- recipes/recipe/urls.py
+++ recipes/recipe/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-	url(r'^(?P<first_name>[A-z ]+)/$', views.recipe, name='recipe'),
+	url(r'^(?P<name>[A-z ]+)/$', views.recipe, name='recipe'),
 ]
--- recipes/recipe/views.py
+++ recipes/recipe/views.py
@@ -5,15 +5,16 @@ from django.views import generic
 from urllib import quote  
 from urllib import urlopen
 import json
-import redis
+from pymongo import Connection
+from pymongo.errors import ConnectionFailure
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r = Connection(host="localhost", port=27017)
 
 def index(request):
 	return render(request, 'recipe/index.html', {})
     #return render(request, 'jokes/index.html')
 
-def joke(request, first_name, last_name):
+def joke(request, name, calorien, ingredienten, tijd):
 	url = 'http://api.icndb.com/jokes/random?firstName=' + quote(first_name) + '&lastName=' + quote(last_name)
 	response = urlopen(url)
 	obj = response.read().decode("utf8")
